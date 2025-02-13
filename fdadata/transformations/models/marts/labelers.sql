7dadca62458734c48be41b807eef2f797c9c340a
Piotr Mierzejewski <piotrmierzejewski@MacBook-Pro-Piotr.local>
2025-02-13 11:53:41 +0100

    Refactor the modeles and project overall to align with the dbt best practices

--- fdadata/transformations/models/marts/labelers.sql
+++ fdadata/transformations/models/marts/labelers.sql
@@ -3,9 +3,9 @@ select
     count(distinct dm.product_id) as total_drugs,
     count(distinct gdi.product_id) as generic_drugs_count,
     count(distinct dai.name) as distinct_active_ingredients
-from {{ ref('drug_labeler') }} as dm
-left join {{ ref('generic_drug_ids') }} as gdi
+from {{ ref('int_ndc_labelers_deduplicated') }} as dm
+left join {{ ref('int_generic_drug_identificated') }} as gdi
     on dm.product_id = gdi.product_id
-left join {{ ref('drug_active_ingredients') }} as dai
+left join {{ ref('int_drug_active_ingredients_unnested') }} as dai
     on dm.product_id = dai.product_id
 group by dm.labeler_name
